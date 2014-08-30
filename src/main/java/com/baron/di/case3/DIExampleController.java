436aab2270d59df03a82aa2ff339bc751ef07318
Administrator <Administrator@MSDN-SPECIAL>
2014-08-30 22:29:53 +0900

    회원가입,로그인 SpringMVC로 만들어보기(파일입출력)

--- src/main/java/com/baron/di/case3/DIExampleController.java
+++ src/main/java/com/baron/di/case3/DIExampleController.java
@@ -8,7 +8,7 @@ public class DIExampleController {
 	
 	// DI기술 없이 서비스 객체가 필요할때
 	// 내부에 만들거나
-//	private DIExampleService service = new DIExampleService();
+	//private DIExampleService service = new DIExampleService();
 	
 	// 생성자에서 만들거나..
 	/*public DIExampleController() {
@@ -17,14 +17,14 @@ public class DIExampleController {
 	
 	// DI가 있다면..?
 	// 어딘가에서 서비스를 주겠지
-	/*public DIExampleController(DIExampleService service) {
-		this.service = service;
-	}*/
+//	public DIExampleController(DIExampleService service) {
+//		this.service = service;
+//	}
 	
 	// setter를 통해서 받을수도 있다
-	/*public void setService(DIExampleService service) {
+	public void setService(DIExampleService service) {
 		this.service = service;
-	}*/
+	}
 	
 	public DIExampleService getService() {
 		return service;
